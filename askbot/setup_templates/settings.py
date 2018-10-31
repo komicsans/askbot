acccb0a9f4e1e266227f3b701bd50b3c7a31a292
mijuanlo <>
2018-10-31 13:57:42 +0100

    Fix default language setting into true settings file

--- askbot/setup_templates/settings.py
+++ askbot/setup_templates/settings.py
@@ -52,8 +52,8 @@ SITE_ID = 1
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
 USE_I18N = True
-LANGUAGE_CODE = 'es'
-LANGUAGES = (('es', 'Spanish'),)
+LANGUAGE_CODE = 'en'
+LANGUAGES = (('en', 'English'),)
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
